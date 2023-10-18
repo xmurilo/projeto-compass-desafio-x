b494a3286ed3f175adbd8dcc4eae96b2b18a42b0
Murilo <dixmurilox@gmail.com>
2023-10-18 18:04:01 -0300

    feat  login with cookie incomplete

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -5,43 +5,54 @@ import Form from '../styledComponents/Form/Form.styled';
 import CustomInput from '../UI/InputUI/CustomInput';
 import ButtonForm from '../styledComponents/ButtonFormStyled/ButtonForm.styled';
 import ButtonFormWrapper from '../styledComponents/ButtonFormStyled/ButtonFormWrapper.styled';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../services/firebaseConfig';
 
 const LoginForm: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
-
   const router = useNavigate();
 
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+  // const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    signInWithEmailAndPassword(email, password);
+    await signInWithEmailAndPassword(auth, email, password)
+      .then(user => {
+        console.log(user);
+        // * Procurar lib de cookies
+        // * Salvar Access token nos cookies 
+        // * Salvar uid nos cookies
+        // * na tela User useEffect com função fetch(method:"get") para buscar dados do usuario no ID 
+        router('/user');
+      })
+      .catch(err => {
+        alert(err);
+      });
   };
 
-  if (loading) {
-    return (
-      <p
-        style={{
-          width: '100vw',
-          height: '100vh',
-          backgroundColor: '#000',
-          color: 'white',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        Carregando
-      </p>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <p
+  //       style={{
+  //         width: '100vw',
+  //         height: '100vh',
+  //         backgroundColor: '#000',
+  //         color: 'white',
+  //         display: 'flex',
+  //         justifyContent: 'center',
+  //         alignItems: 'center',
+  //       }}
+  //     >
+  //       Carregando
+  //     </p>
+  //   );
+  // }
 
-  if (user) {
-    console.log(user);
-  }
+  // if (user) {
+  //   console.log(user);
+  // }
 
   return (
     <Form className='formLogin' onSubmit={handleLogin}>
