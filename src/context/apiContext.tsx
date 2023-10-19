2bc9370b9cff2f8ec4a41dddbedcbe5a2d15a1d2
Murilo <dixmurilox@gmail.com>
2023-10-19 18:27:01 -0300

    feat apiContext

--- src/context/apiContext.tsx
+++ src/context/apiContext.tsx
@@ -1,41 +1,55 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 
-const ApiContext = createContext(null);
+const ApiContext = createContext<IUserData>({} as IUserData);
 
 export function useApi() {
-  return useContext(ApiContext);
+  const context = useContext(ApiContext);
+
+  return context;
 }
 
 interface IApi {
   children: React.ReactNode;
 }
 
-interface IUserData  {
-  name?: string;
-  email?: string;
-  password?: string;
-  birthDate?: number;
-  country?: string;
-  profession?: string;
-  relationship?: string;
-};
+interface IUserData {
+  name: string;
+  email: string;
+  password: string;
+  birthDate: number;
+  country: string;
+  profession: string;
+  relationship: string;
+}
 
 export const ApiProvider: React.FC<IApi> = ({ children }) => {
-  const [userData, setUserData] = useState<IUserData>();
+  const [userData, setUserData] = useState<IUserData>({} as IUserData);
 
   useEffect(() => {
     const uid = Cookies.get('uid');
     const accessToken = Cookies.get('accessToken');
-    if (uid) {
+
+    if (uid && accessToken) {
       fetch(`http://localhost:3333/users/${uid}`, {
         method: 'GET',
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
       })
-        .then(res => res.json())
-        .then(data => setUserData(data));
+        .then(res => {
+          if (!res.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return res.json();
+        })
+        .then(data => {
+          // console.log(data);
+          setUserData(data);
+        })
+        .catch(error => {
+          console.error('Error fetching user data:', error);
+        });
     }
   }, []);
 
