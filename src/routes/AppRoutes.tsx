82f287e68a4eb4c793407ac7b8a6c94e7e7866c3
Murilo <dixmurilox@gmail.com>
2023-10-20 17:54:50 -0300

    Refactor: appRoutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import RootLayout from './pages/Root';
-import UserProfile from './pages/UserProfile';
-import GlobalStyle from './styles/globalStyles';
-import LoginPage from './pages/Login';
-import Register from './pages/Register';
-import EditUserProfile from './pages/EditUserProfile';
-import RecoverPasswordPage from './pages/RecoverPassword';
-import ChangePassword from './pages/ChangePassword';
+import RootLayout from '../pages/Root';
+import UserProfile from '../pages/UserProfile';
+import GlobalStyle from '../styles/globalStyles';
+import LoginPage from '../pages/Login';
+import Register from '../pages/Register';
+import EditUserProfile from '../pages/EditUserProfile';
+import RecoverPasswordPage from '../pages/RecoverPassword';
+import ChangePassword from '../pages/ChangePassword';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
-
 const uid = Cookies.get('uid');
 const accessToken = Cookies.get('accessToken');
 const auth = uid && accessToken ? true : false;
