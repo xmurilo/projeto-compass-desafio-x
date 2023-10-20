060874cc10fe44b983eef14fdbf7e5056236f277
Murilo <dixmurilox@gmail.com>
2023-10-20 17:55:28 -0300

    Feat: util splitBirthDate

---
+++ src/utils/splitBirthDate.tsx
@@ -0,0 +1,21 @@
+export const splitBirthDate = (dateString?: string): string => {
+  if (!dateString) {
+    return 'Preencha seu data de nascimento';
+  }
+  const [year, month, day] = dateString.split('-');
+  const monthNames = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ];
+  return `${day} de ${monthNames[parseInt(month) - 1]} de ${year}`;
+};
